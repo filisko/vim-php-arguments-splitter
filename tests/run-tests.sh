3aa062d0341e134d0728ca4319819ce49fe0da37
Filis Futsarov <filisfutsarov@gmail.com>
2020-12-08 15:52:34 +0100

    update

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -32,14 +32,4 @@ set -p
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
 
 : "${VADER_TEST_VIM:=vim}"
-eval "$VADER_TEST_VIM -Nu <(cat <<VIMRC
-    filetype off
-    set shiftwidth=4
-
-    set rtp+=vader.vim
-    set rtp+=..
-
-    filetype plugin indent on
-    syntax enable
-VIMRC
-) -c 'Vader! *.vader'"
+eval "$VADER_TEST_VIM -Nu vimrc -c 'Vader! *'"
